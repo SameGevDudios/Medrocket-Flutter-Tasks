e17b3a61bb6b03d7ae1e9a23038b556a49550168
SameGevDudios <magomed.chapaev1@gmail.com>
2025-11-05 17:38:18 +0300

    Add repository realization

---
+++ medrocket_task_2/lib/data/repositories/poll_repository_mock.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'poll_repository.dart';
+import '../models/poll.dart';
+
+class PollRepositoryMock implements PollRepository{
+  final Duration delay = const Duration(seconds: 2);
+  final int percent;
+
+  PollRepositoryMock({required this.percent});
+
+  @override
+  Future<Poll> fetchPoll() async {
+    await Future.delayed(delay);
+    return Poll(percent: percent.clamp(0, 100));
+  }
+}
